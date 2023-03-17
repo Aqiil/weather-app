6fd4a70dd8ca37b5a2b11e1ae31e0aa0617d50e7
Abubakar Aqiil <abubakar.aqiil@gmail.com>
2023-03-17 15:51:04 +0000

    Make weather icon dynamic based on desc

--- src/components/MainPane/index.js
+++ src/components/MainPane/index.js
@@ -1,4 +1,12 @@
-import demo_icon from '../../assets/icons/weather/demo.png';
+import broken_clouds from '../../assets/icons/weather/broken_clouds.png';
+import clear_sky from '../../assets/icons/weather/clear_sky.png';
+import few_clouds from '../../assets/icons/weather/few_clouds.png';
+import mist from '../../assets/icons/weather/mist.png';
+import rain from '../../assets/icons/weather/rain.png';
+import scattered_clouds from '../../assets/icons/weather/scattered_clouds.png';
+import shower_rain from '../../assets/icons/weather/shower_rain.png';
+import snow from '../../assets/icons/weather/snow.png';
+import thunderstorm from '../../assets/icons/weather/thunderstorm.png';
 
 import './style.css';
 import React, { useState, useEffect } from 'react';
@@ -9,6 +17,18 @@ function MainPane() {
   const formattedDate = today.toLocaleDateString('en-US', options);
 
 	const [weatherData, setWeatherData] = useState(null);
+
+	const iconMap = {
+    'clear sky': clear_sky,
+    'few clouds': few_clouds,
+    'scattered clouds': scattered_clouds,
+    'broken clouds': broken_clouds,
+    'shower rain': shower_rain,
+    'rain': rain,
+    'thunderstorm': thunderstorm,
+    'snow': snow,
+    'mist': mist
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,7 +49,7 @@ function MainPane() {
 			<div className="container-wrapper">
 				<div className="main-container">
 					{/* Current weather condition icon */}
-					<img src={demo_icon} className="demo-icon" alt="partly cloudy" />
+					<img src={iconMap[weatherData?.desc]} className="weather-icon" alt="weather condition" />
 
 					{/* Conditional check to verify weatherData is not null */}
 					{weatherData ? (
