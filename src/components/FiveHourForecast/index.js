25f86060e658244704ba81480eefce0808e5d18e
Abubakar Aqiil <abubakar.aqiil@gmail.com>
2023-03-18 06:51:54 +0000

    Add FivehourForecast component base

---
+++ src/components/FiveHourForecast/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+import React, { useState, useEffect } from 'react';
+
+function FiveHourForecast() {
+
+    return (
+        
+    );
+  }
+  
+  export default FiveHourForecast;  
