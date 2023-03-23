728584c3a99b591ecf7de39a84b3879bd526c0db
Abubakar Aqiil <abubakar.aqiil@gmail.com>
2023-03-23 06:03:36 +0000

    Integrate API for 5 day forecast

--- src/components/FiveDayForecast/index.js
+++ src/components/FiveDayForecast/index.js
@@ -6,14 +6,16 @@ import config from '../../config.js';
 
 function FiveDayForecast() {
   const [dailyData, setDailyData] = useState(null);
+  const [hourlyData, setHourlyData] = useState(null);
 
   useEffect(() => {
-    const fetchDailyData = async () => {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${config.DEFAULT_COUNTRY}&units=metric&APPID=${config.API_KEY}`);
+    const fetchWeatherData = async () => {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${config.DEFAULT_LATITUDE}&lon=${config.DEFAULT_LONGITUDE}&exclude=minutely,alerts&units=metric&appid=${config.API_KEY}`);
       const data = await response.json();
-      setDailyData(data.list.filter((_, index) => index % 8 === 0).slice(0, 6));
+      setDailyData(data.daily.slice(0, 5));
+      setHourlyData(data.hourly);
     };
-    fetchDailyData();
+    fetchWeatherData();
   }, []);
 
   const formatDay = (timestamp) => {
@@ -22,16 +24,43 @@ function FiveDayForecast() {
     return date.toLocaleDateString('en-US', options);
   };
 
+  const findNextRainTimeAndCoverage = (dayTimestamp) => {
+    if (!hourlyData) return { time: null, coverage: null };
+
+    const dayDate = new Date(dayTimestamp * 1000);
+    const nextRain = hourlyData.find(item => {
+      const itemDate = new Date(item.dt * 1000);
+      return itemDate.getDate() === dayDate.getDate() && item.weather[0].main === 'Rain';
+    });
+
+    if (!nextRain) return { time: null, coverage: null };
+
+    return {
+      time: new Date(nextRain.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+      coverage: Math.round(nextRain.pop * 100),
+    };
+  };
+
   return (
     <div className="five-day-forecast-container-wrapper">
       <p className="five-day-forecast-title">5-day Forecast</p>
       <hr />
       <div className="five-day-forecast-container">
-        {dailyData &&
-          dailyData.map((dayData, index) => (
-            <DayForecast key={index} day={index === 0 ? 'Today' : formatDay(dayData.dt)} desc={dayData.weather[0].main.toLowerCase()} coverage={dayData.clouds.all} high={Math.round(dayData.main.temp_max)} low={Math.round(dayData.main.temp_min)}
-            />
-          ))}
+        {dailyData && hourlyData &&
+          dailyData.map((dayData, index) => {
+            const { time, coverage } = findNextRainTimeAndCoverage(dayData.dt);
+            return (
+              <DayForecast
+                key={index}
+                day={index === 0 ? 'Today' : formatDay(dayData.dt)}
+                desc={dayData.weather[0].main.toLowerCase()}
+                coverage={coverage}
+                time={time}
+                high={Math.round(dayData.temp.max)}
+                low={Math.round(dayData.temp.min)}
+              />
+            );
+          })}
       </div>
     </div>
   );
--- src/components/MainPane/index.js
+++ src/components/MainPane/index.js
@@ -13,13 +13,13 @@ import config from '../../config';
 import React, { useState, useEffect } from 'react';
 
 function MainPane({ onClick }) {
-	const today = new Date();
+  const today = new Date();
   const options = { weekday: 'long', day: 'numeric', month: 'long' };
   const formattedDate = today.toLocaleDateString('en-UK', options);
 
-	const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
 
-	const iconMap = {    
+  const iconMap = {
     'Thunderstorm': thunderstorm,
     'Drizzle': drizzle,
     'Rain': rain,
@@ -30,73 +30,56 @@ function MainPane({ onClick }) {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${config.DEFAULT_COUNTRY}&units=metric&APPID=${config.API_KEY}`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${config.DEFAULT_LATITUDE}&lon=${config.DEFAULT_LONGITUDE}&exclude=minutely,alerts&units=metric&appid=${config.API_KEY}`);
       const data = await response.json();
 
-      const currentTime = Math.floor(new Date().getTime() / 1000);
-      // Filter data for forecasts for today only
-      const today = data.list.filter(item => {
-        const itemTime = item.dt;
-        return new Date(itemTime * 1000).toDateString() === new Date(currentTime * 1000).toDateString();
-      });
-      
-      // Find the next rain forecast
-      const nextRain = data.list.find(item => item.weather[0].main === 'Rain');
+      const nextRain = data.hourly.find(item => item.weather[0].main === 'Rain');
       const nextRainTime = nextRain ? new Date(nextRain.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : null;
-      // Convert rain coverage to percentage or set to null if no rain forecast
       const rainCoverage = nextRain ? Math.round(nextRain.pop * 100) : null;
-      
-      // Find the highest and lowest temperature for today
-      const tempArray = today.map(item => item.main.temp);
-      const high = Math.round(Math.max(...tempArray));
-      const low = Math.round(Math.min(...tempArray));
-      
-      // console.log(tempArray)
-      // console.log(high, low);
 
       setWeatherData({
-        desc: data.list[0].weather[0].main,
-        loc: data.city.name,
-        temp: Math.round(data.list[0].main.temp),
+        desc: data.current.weather[0].main,
+        loc: config.DEFAULT_COUNTRY,
+        temp: Math.round(data.current.temp),
         nextRainTime,
         rainCoverage,
-        high,
-        low
+        high: Math.round(data.daily[0].temp.max),
+        low: Math.round(data.daily[0].temp.min),
       });
     }
     fetchData();
   }, []);
 
-	return (
-		<>
-			{/* Main pane container */}
-			<div className="main-pane-container-wrapper" onClick={onClick}>
-				<div className="main-pane-container">
-					{/* Current weather condition icon */}
-					<img src={iconMap[weatherData?.desc]} className="weather-icon" alt="weather condition" />
+  return (
+    <>
+      {/* Main pane container */}
+      <div className="main-pane-container-wrapper" onClick={onClick}>
+        <div className="main-pane-container">
+          {/* Current weather condition icon */}
+          <img src={iconMap[weatherData?.desc]} className="weather-icon" alt="weather condition" />
 
-					{/* Conditional check to verify weatherData is not null */}
-					{weatherData ? (
-						<>
-						{/* Weather info */}
-						<h1>{weatherData.temp}</h1>
-						<h3>{weatherData.loc}</h3>
-						<h2 className='capitalize'>{weatherData.desc}</h2>
-						<h3>{formattedDate}</h3>
-						</>
-					) : (
-					  <p>Loading weather data...</p>
-					)}
-				</div>
-				{/* Daily forecast pane container */}
-				<hr className='main-pane-hr'/>
-				<div className="main-pane-sub-container">
+          {/* Conditional check to verify weatherData is not null */}
+          {weatherData ? (
+            <>
+            {/* Weather info */}
+            <h1>{weatherData.temp}</h1>
+            <h3>{weatherData.loc}</h3>
+            <h2 className='capitalize'>{weatherData.desc}</h2>
+            <h3>{formattedDate}</h3>
+            </>
+          ) : (
+            <p>Loading weather data...</p>
+          )}
+        </div>
+        {/* Daily forecast pane container */}
+        <hr className='main-pane-hr'/>
+        <div className="main-pane-sub-container">
           <DailyRainfall coverage={weatherData?.rainCoverage} time={weatherData?.nextRainTime} />
           <DailyHL high={weatherData?.high} low={weatherData?.low} />
         </div>
-			</div>
-		</>
-	)
+      </div>
+    </>
+  )
 }
 
 export default MainPane;
